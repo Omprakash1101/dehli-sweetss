7bc627799773a5a82542fd9b38e7a451a3e5f141
Omprakash G <108943224+Omprakash1101@users.noreply.github.com>
2024-12-27 11:35:27 +0000

    modified:   api/settings.py 	new file:   example/templates/example/base.html 	modified:   example/views.py

--- example/views.py
+++ example/views.py
@@ -1,16 +1,18 @@
 # example/views.py
 from datetime import datetime
-
+from django.template import loader
 from django.http import HttpResponse
 
 def index(request):
-    now = datetime.now()
-    html = f'''
+    template = loader.get_template('example/base.html')
+    con={
+    'now':datetime.now(),
+    'html':f'''
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <p>The current time is .</p>
         </body>
     </html>
-    '''
-    return HttpResponse(html)
+    ''',}
+    return HttpResponse(template.render(con, request))
